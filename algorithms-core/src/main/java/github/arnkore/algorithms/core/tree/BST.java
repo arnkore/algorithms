c7c279ea91aa323047cfbbcd7de722502d8fd048
arnkore <ludo.arnk@gmail.com>
2015-08-24 16:49:05 +0800

    二叉搜索树

--- algorithms-core/src/main/java/github/arnkore/algorithms/core/tree/BST.java
+++ algorithms-core/src/main/java/github/arnkore/algorithms/core/tree/BST.java
@@ -1,151 +1,315 @@
 package github.arnkore.algorithms.core.tree;
 
 import github.arnkore.algorithms.core.exception.EmptySymbolTableException;
+import github.arnkore.algorithms.core.queue.Queue;
+import github.arnkore.algorithms.core.queue.impl.ArrayQueue;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import com.google.common.base.Joiner;
 
 /**
- * 符号表-二叉搜索树实现
+ * 二叉搜索树
  * @author arnkore
  *
- * @param <Key>
- * @param <Value>
+ * @param <K>
+ * @param <V>
  */
-public class BST<Key extends Comparable<Key>, Value> {
-	private Node root; // 根节点
+public class BST<K extends Comparable<K>, V> implements Iterable<K> {
+	private Node root;
 	
-	private class Node {
-		private Key key;
-		private Value value;
-		private Node left, right;
-		private int size;
-		
-		public Node(Key key, Value value, int size) {
-			super();
-			this.key = key;
-			this.value = value;
-			this.size = size;
-		}
+	/**
+	 * 判断是否为空的二叉搜索树
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return root == null;
 	}
 	
 	/**
-	 * 在符号表中查找键key对应的值
+	 * 查找是否存在键key
 	 * @param key
 	 * @return
 	 */
-	public Value get(Key key) {
-		Node ptr = root;
-		
-		while (ptr != null) {
-			int cmp = key.compareTo(ptr.key);
-			if (cmp < 0) ptr = ptr.left;
-			else if (cmp > 0) ptr = ptr.right;
-			else return ptr.value;
+	public V get(K key) {
+		Node cur = root;
+		while (cur != null) {
+			int cmp = key.compareTo(cur.key);
+			
+			if (cmp < 0) cur = cur.left;
+			else if (cmp > 0) cur = cur.right;
+			else return cur.val; // search hit
 		}
 		
-		return null;
+		return null; // search miss
 	}
 	
 	/**
-	 * 在符号表中更新键key上对应的值
+	 * 插入键值对key:val，如果已存在该键，则更新其上的值。
 	 * @param key
-	 * @param value
+	 * @param val
 	 */
-	public void put(Key key, Value value) {
-		put(root, key, value);
-	}
-	
-	private Node put(Node ptr, Key key, Value value) {
-		if (ptr == null) return new Node(key, value, 1);
-		
-		int cmp = key.compareTo(ptr.key);
-		if (cmp < 0) ptr.left = put(ptr.left, key, value);
-		else if (cmp > 0) ptr.right = put(ptr.right, key, value);
-		else ptr.value = value;
-		
-		ptr.size += 1;
-		return ptr;
+	public void put(K key, V val) {
+		root = put(root, key, val);
 	}
 	
 	/**
-	 * 符号表中最小的键
+	 * 在二叉搜索子树subroot中插入键值对key:val，如果二叉搜索子树中已存在该键，则更新其上的值。
+	 * @param subroot 二叉搜索树子树
+	 * @param key
+	 * @param val
 	 * @return
 	 */
-	public Key min() {
+	private Node put(Node subroot, K key, V val) {
+		if (subroot == null) return new Node(key, val);
+		
+		int cmp = key.compareTo(subroot.key);
+		if (cmp < 0) subroot.left = put(subroot.left, key, val);
+		else if (cmp > 0) subroot.right = put(subroot.right, key, val);
+		else subroot.val = val; // search hit, update node's val field
+		
+		subroot.size = size(subroot.left) + size(subroot.right) + 1;
+		return subroot;
+	}
+	
+	/**
+	 * 删除存储键key的节点
+	 * @param key
+	 */
+	public void delete(K key) {
+		if (root == null) throw new EmptySymbolTableException();
+		root = delete(root, key);
+	}
+	
+	/**
+	 * 在二叉搜索子树subroot中删除存储键key的节点
+	 * @param subroot
+	 * @param key
+	 * @return
+	 */
+	private Node delete(Node subroot, K key) {
+		if (subroot == null) throw new NoSuchElementException();
+		
+		int cmp = key.compareTo(subroot.key);
+		if (cmp < 0) {
+			subroot.left = delete(subroot.left, key);
+		} else if (cmp > 0) {
+			subroot.right = delete(subroot.right, key);
+		} else {
+			if (subroot.right == null) return subroot.left;
+			if (subroot.left == null) return subroot.right;
+			
+			Node rminNode = min(subroot.right);
+			rminNode.right = delMin(subroot.right);
+			rminNode.left = subroot.left;
+			subroot = rminNode;
+		}
+		
+		subroot.size = size(subroot.left) + size(subroot.right) + 1;
+		return subroot;
+	}
+	
+	/**
+	 * 返回最小的键
+	 * @return
+	 */
+	public K min() {
 		if (root == null) throw new EmptySymbolTableException();
 		return min(root).key;
 	}
 	
-	private Node min(Node x) {
-		while (x.left != null) x = x.left;
-		return x;
+	/**
+	 * 返回二叉搜索子树subroot中key最小的节点
+	 * @param subroot
+	 * @return
+	 */
+	private Node min(Node subroot) {
+		while (subroot.left != null) {
+			subroot = subroot.left;
+		}
+		
+		return subroot;
 	}
 	
 	/**
-	 * 符号表中最大的键
+	 * 返回最大的键
 	 * @return
 	 */
-	public Key max() {
+	public K max() {
 		if (root == null) throw new EmptySymbolTableException();
 		return max(root).key;
 	}
 	
-	private Node max(Node x) {
-		while (x.right != null) x = x.right;
-		return x;
-	}
-	
 	/**
-	 * 在符号表中删除键key对应的键值对
-	 * @param key
+	 * 返回二叉搜索子树subroot中存储最大键的那个节点
+	 * @param subroot
+	 * @return
 	 */
-	public void delete(Key key) {
-		delete(root, key);
-	}
-	
-	private Node delete(Node x, Key key) {
-		if (x == null) return null;
-		
-		int cmp = key.compareTo(x.key);
-		if (cmp < 0) x.left = delete(x.left, key);
-		else if (cmp > 0) x.right = delete(x.right, key);
-		else {
-			if (x.right == null) return x.left;
-			if (x.left == null) return x.right;
-			Node tmp = x;
-			// 查找右子树中最小的节点，并用该节点替换待删除节点
-			x = min(x.right); 
-			x.right = deleteMin(tmp.right);
-			x.left = tmp.left;
+	private Node max(Node subroot) {
+		while (subroot.right != null) {
+			subroot = subroot.right;
 		}
 		
-		x.size = size(x.left) + size(x.right) + 1;
-		return x;
+		return subroot;
 	}
 	
 	/**
-	 * 在符号表中删除最小的键所对应的键值对
+	 * 删除存储最小键的那个节点
 	 */
-	public void deleteMin() {
+	public void delMin() {
 		if (root == null) throw new EmptySymbolTableException();
-		deleteMin(root);
+		root = delMin(root);
 	}
 	
-	private Node deleteMin(Node x) {
-		if (x.left == null) return x.right;
-		x.left = deleteMin(x.left);
-		x.size = size(x.left) + size(x.right) + 1;
-		return x;
+	/**
+	 * 删除二叉搜索子树subroot中的存储最小键的那个节点
+	 * @param subroot
+	 * @return
+	 */
+	private Node delMin(Node subroot) {
+		if (subroot.left != null) {
+			subroot.left = delMin(subroot.left);
+			subroot.size = size(subroot.left) + size(subroot.right) + 1;
+			return subroot;
+		} else { // search hit, this node is min key node.
+			return subroot.right;
+		}
+	}
+	
+	/**
+	 * 删除存储最大键的那个节点
+	 */
+	public void delMax() {
+		if (root == null) throw new EmptySymbolTableException();
+		root = delMax(root);
+	}
+	
+	/**
+	 * 删除二叉搜索子树中键最大的那个节点
+	 * @param subroot
+	 * @return
+	 */
+	private Node delMax(Node subroot) {
+		if (subroot.right != null) {
+			subroot.right = delMax(subroot.right);
+			subroot.size = size(subroot.left) + size(subroot.right) + 1;
+			return subroot;
+		} else {
+			return subroot.left;
+		}
 	}
 
 	/**
-	 * 符号表包含的节点数
+	 * 返回总的节点数
 	 * @return
 	 */
 	public int size() {
 		return size(root);
 	}
+
+	/**
+	 * 二叉搜索子树subroot含有的节点数
+	 * @param subroot
+	 * @return
+	 */
+	private int size(Node subroot) {
+		return subroot == null ? 0 : subroot.size;
+	}
+
+	public Iterator<K> iterator() {
+		Queue<K> q = new ArrayQueue<K>();
+		inorder(root, q);
+		return q.iterator();
+	}
 	
-	private int size(Node x) {
-		if (x == null) return 0;
-		return x.size;
+	private void inorder(Node subroot, Queue<K> q) {
+		if (subroot == null) return;
+		inorder(subroot.left, q);
+		q.enqueue(subroot.key);
+		inorder(subroot.right, q);
+	}
+	
+	private class Node {
+		private Node left, right;
+		private K key;
+		private V val;
+		private int size;
+		
+		public Node(K key, V val, int size) {
+			this.key = key;
+			this.val = val;
+			this.size = size;
+		}
+		
+		public Node(K key, V val) {
+			this(key, val, 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		BST<String, Integer> bst = new BST<String, Integer>();
+		bst.put("a", 1);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("b", 2);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("c", 3);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("d", 4);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("e", 5);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("e");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("g", 6);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("h", 7);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("i", 8);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("j", 9);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("a");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("k", 10);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.put("l", 11);
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		System.out.println(bst.get("a"));
+		System.out.println(bst.get("b"));
+		System.out.println(bst.get("j"));
+		System.out.println(bst.min());
+		System.out.println(bst.max());
+		bst.delMin();
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delMax();
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		System.out.println(bst.min());
+		System.out.println(bst.max());
+		
+//		while (!bst.isEmpty()) {
+//			bst.delMin();
+//			System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+//		}
+		
+//		while (!bst.isEmpty()) {
+//			bst.delMax();
+//			System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+//		}
+		
+		bst.delete("g");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("h");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("j");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("d");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("i");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("c");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
+		bst.delete("k");
+		System.out.println(Joiner.on(" ").join(bst) + " " + bst.size());
 	}
 }
